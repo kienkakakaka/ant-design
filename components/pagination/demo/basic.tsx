f302f8ca12af96cf42251ab3cf329625bca626dc
kienkakakaka <51254936+kienkakakaka@users.noreply.github.com>
2023-10-15 04:47:02 +0700

    Update basic.tsx
    Signed-off-by: kienkakakaka <51254936+kienkakakaka@users.noreply.github.com>

--- components/pagination/demo/basic.tsx
+++ components/pagination/demo/basic.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Pagination } from 'antd';
 
-const App: React.FC = () => <Pagination defaultCurrent={1} total={50} />;
+const App: React.FC = () => <Pagination defaultCurrent={1} total={500} />;
 
 export default App;
